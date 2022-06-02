e385d495f91b8f5a69832987e1dde403daed20da
GetieBalew24 <getiebalew2020@gmail.com>
2022-06-02 13:31:51 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,22 @@
-#include<stdio.h>
+#include <stdio.h>
+
+
+
 /**
- * my_function - This is a description
- */
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
+
 int main(void)
 
 {
 	puts("\"Programming is like building a multilingual puzzle\n");
-       	return (0);
+
+	    return (0);
+
 }
