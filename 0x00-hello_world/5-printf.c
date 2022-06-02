40e6cec0f189c1cf22880ddeb8312f41541f0f77
GetieBalew24 <getiebalew2020@gmail.com>
2022-06-02 15:36:10 -0700

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 
 /**
-* 	main - entry point
+* main - entry point
 *
-* 	Return : always 0 (sucess)
+* Return : always 0 (sucess)
 */
-int main (void)
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+		return (0);
 }
