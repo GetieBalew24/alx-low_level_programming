7180fac41b54bd4a9240e61f1fc3e71ca06d777b
GetieBalew24 <getiebalew2020@gmail.com>
2022-06-02 14:06:54 -0700

    Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * my main function
+ * to print text using printf
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
