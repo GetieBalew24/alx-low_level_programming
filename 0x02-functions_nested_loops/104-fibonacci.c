64eef53f95eca756c52df8abd20ac65e624d79bc
GetieBalew24 <getiebalew2020@gmail.com>
2022-06-08 00:40:15 -0700

    Task 16

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,47 +1,26 @@
-#include <limits.h>
-#include <math.h>
 #include <stdio.h>
 
 /**
- * main - entry point
+ * main - Prints the first 98 Fibonacci numbers, starting with
+ *  1 and 2, separated by a comma followed by a space.
  *
  * Return: Always 0.
  */
 int main(void)
 {
-	int i;
-	int j;
-	long a_lo = 1;
-	long b_lo = 2;
-	long a_hi = 0;
-	long b_hi = 0;
-	int limit_len = floor(log10(LONG_MAX / 2));
-	long limit = pow(10, limit_len);
+	int f1 = 0, f2 = 1, i;
 
-	for (i = 0; i < 98; ++i)
+	if (n < 1)
+		return;
+	printf("%lu, ", f1);
+	for (i = 1; i < 98; i++)
 	{
-		if (a_hi)
-		{
-			printf("%ld", a_hi);
-			for (j = floor(log10(a_lo)) + 1; j < limit_len; ++j)
-				putchar('0');
-		}
-		printf("%ld", a_lo);
-		b_lo = b_lo + a_lo;
-		a_lo = b_lo - a_lo;
-		a_hi = b_hi - a_hi;
-		b_hi = b_hi + a_hi;
-		if (b_lo >= limit)
-		{
-			b_hi += b_lo / limit;
-			b_lo %= limit;
-			a_hi += a_lo / limit;
-			a_lo %= limit;
-		}
-		if (i < 97)
-			printf(", ");
+		printf("%lu, ", f2); 
 
+		int next = f1 + f2;
+
+		f1 = f2;
+		f2 = next;
 	}
-	putchar('\n');
 	return (0);
 }
