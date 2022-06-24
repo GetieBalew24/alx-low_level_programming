3d9627ee730b5cffcd82e6aa8dc846f416206b41
GetieBalew24 <getiebalew2020@gmail.com>
2022-06-24 13:09:17 -0700

    Tasks about static library

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isupper - define if a character is uppercase
+ *
+ * Description: Prints the alphabet with _putchar
+ * @c: charater
+ * Return: void
+ */
+
+int _isupper(int c)
+{
+	return (c >= 65 && c <= 90);
+}
