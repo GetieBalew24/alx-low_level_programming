cc69efc26b2e878740fa02a78a6f34f71491485a
GetieBalew24 <getiebalew2020@gmail.com>
2022-07-06 09:32:23 -0700

    Task0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,13 +1,15 @@
 #include "function_pointers.h"
+#include <stdlib.h>
 
 /**
- * print_name - prints a name
- * @name: string
+ * print_name - prints the name
+ * @name: name
  * @f: function
  */
 
 void print_name(char *name, void (*f)(char *))
 {
-	if (f != NULL)
-		f(name);
+	if (name == NULL || f == NULL)
+		return;
+	f(name);
 }
