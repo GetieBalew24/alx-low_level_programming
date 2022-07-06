046e2ff402d57161c8024f7015f89db15c10fffe
GetieBalew24 <getiebalew2020@gmail.com>
2022-07-06 09:59:24 -0700

    Tasks

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,9 +1,8 @@
-#ifndef VARIADIC_FUNCTIONS_H
-#define VARIADIC_FUNCTIONS_H
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+#include <stddef.h>
 
-#include <stdarg.h>
-
-int _putchar(char c);
+int  _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 
