272d7585741fb63ea8d8425b6b7bc53165c39b33
GetieBalew24 <getiebalew2020@gmail.com>
2022-07-25 04:32:08 -0700

    Header

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H_FILE_IO
+#define MAIN_H_FILE_IO
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+int _putchar(char c);
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif
